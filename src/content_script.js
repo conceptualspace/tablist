68db3852ca5c4685aa0de3817683e14c7158aa89
conceptualspace <dev@conceptualspace.net>
2017-09-17 23:17:16 -0400

    sanitize input

--- src/content_script.js
+++ src/content_script.js
@@ -3,15 +3,11 @@ window.browser = (function () {
     return window.browser || window.chrome;
 })();
 
-var tabList = '';
-
 browser.tabs.query({}, function(tabs) {
     for (var i=0; i<tabs.length; ++i) {
         if ( !tabs[i].url.startsWith("chrome") && !tabs[i].url.startsWith("moz") && !tabs[i].url.startsWith("about") ) {
-            tabList += '<a href="' + tabs[i].url + '">' + tabs[i].url + '</a><br>';
+            document.getElementById("list").appendChild(document.createTextNode(tabs[i].url));
+            document.getElementById("list").appendChild(document.createElement('br'));
         }
     }
-
-    document.getElementById("list").innerHTML = tabList;
-
 });
