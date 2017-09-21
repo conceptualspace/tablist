87bad7ab858d55a1d239eb9191811548e5fb9cfb
conceptualspace <dev@conceptualspace.net>
2017-09-21 01:21:58 -0400

    support android
    > firefox on android seems to only return tab data for recently used tabs
    > so we have to "activate" each tab before querying them for their URL
    > active tabs are not focused by default, so hopefully is transparent to user. temporarily added an alert in case it's slow
    > dropped redundant "activetab" permission

--- src/content_script.js
+++ src/content_script.js
@@ -3,12 +3,40 @@ window.browser = (function () {
     return window.browser || window.chrome;
 })();
 
-browser.tabs.query({}, function(tabs) {
-    for (var i=0; i<tabs.length; ++i) {
-        if ( !tabs[i].url.startsWith("chrome") && !tabs[i].url.startsWith("moz") && !tabs[i].url.startsWith("about") ) {
-            var p = document.createElement('p');
-            p.appendChild(document.createTextNode(tabs[i].url));
-            document.getElementById('list').appendChild(p);
+function listTabs() {
+    browser.tabs.query({}, function (tabs) {
+        for (let i = 0; i < tabs.length; ++i) {
+            if (!tabs[i].url.startsWith("chrome") && !tabs[i].url.startsWith("moz") && !tabs[i].url.startsWith("about")) {
+                let p = document.createElement('p');
+                p.appendChild(document.createTextNode(tabs[i].url));
+                document.getElementById('list').appendChild(p);
+            }
         }
-    }
-});
+    });
+}
+
+function activateTabs() {
+    browser.tabs.query({}, async function(tabs) {
+        for (let i=0; i<tabs.length; ++i) {
+            await browser.tabs.update(tabs[i].id, {
+                active: true
+            });
+        }
+        listTabs()
+    });
+}
+
+function init() {
+    browser.runtime.getPlatformInfo(function (platform) {
+        if (platform.os === "android") {
+            // workaround for ff on android
+            // tab.url is only available for tabs that have been recently used. so we'll activate them all before we query them
+            alert("Generating list of tabs. This may take a moment...");
+            activateTabs()
+        } else {
+            listTabs()
+        }
+    });
+}
+
+init();
