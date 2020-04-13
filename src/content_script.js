5b46c5b0a981bb3334111a3b7d7af3a7524ebf88
conceptualspace <dev@conceptualspace.net>
2020-04-13 13:51:23 -0400

    v1.6 - mobile fixes
    - make sure the tablist itself is the foused tab after getting all the urls

--- src/content_script.js
+++ src/content_script.js
@@ -3,14 +3,20 @@ window.browser = (function () {
     return window.browser || window.chrome;
 })();
 
+let tabListTabId = null;
+
 function listTabs() {
+    const list = document.getElementById('list');
     browser.tabs.query({}, function (tabs) {
         for (let i = 0; i < tabs.length; ++i) {
-            if (!tabs[i].url.startsWith("chrome") && !tabs[i].url.startsWith("moz") && !tabs[i].url.startsWith("about")) {
+            if (tabs[i].url && !tabs[i].url.startsWith("chrome") && !tabs[i].url.startsWith("moz") && !tabs[i].url.startsWith("about") && !tabs[i].url.startsWith("data") ) {
                 let p = document.createElement('p');
                 p.appendChild(document.createTextNode(tabs[i].url));
-                document.getElementById('list').appendChild(p);
+                list.appendChild(p);
             }
+        }
+        if (tabListTabId) {
+            browser.tabs.update(tabListTabId, {active:true, highlighted:true});
         }
     });
 }
@@ -27,15 +33,18 @@ function activateTabs() {
 }
 
 function init() {
-    browser.runtime.getPlatformInfo(function (platform) {
-        if (platform.os === "android") {
-            // workaround for ff on android
-            // tab.url is only available for tabs that have been recently used. so we'll activate them all before we query them
-            alert("Generating list of tabs. This may take a moment...");
-            activateTabs()
-        } else {
-            listTabs()
-        }
+    browser.tabs.getCurrent(function(tab) {
+        tabListTabId = tab.id;
+        browser.runtime.getPlatformInfo(function (platform) {
+            if (platform.os === "android") {
+                // workaround for ff on android
+                // tab.url is only available for tabs that have been recently used. so we'll activate them all before we query them
+                alert("Generating list of tabs. This may take a moment...");
+                activateTabs()
+            } else {
+                listTabs()
+            }
+        });
     });
 }
 
